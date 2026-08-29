709bdf124dee342878f22fdd0a825ff1bbe9b2d7
AhmedNext <fexo.4921@gmail.com>
2026-08-29 17:26:58 +0300

    new

---
+++ TMessagesProj/src/main/java/com/exteragram/messenger/boost/encryption/EncryptionHelper.java
@@ -0,0 +1,48 @@
+package com.exteragram.messenger.boost.encryption;
+
+import org.telegram.messenger.MessageObject;
+
+public class EncryptionHelper {
+    public static String[] names = {"None"};
+    
+    public static boolean isEncrypted(String message) {
+        return false;
+    }
+    
+    public static boolean isEncrypted(MessageObject messageObject, Object obj) {
+        return false;
+    }
+    
+    public static MessageObject decryptMessage(MessageObject messageObject) {
+        return messageObject;
+    }
+    
+    public static MessageObject decryptMessage(MessageObject messageObject, Object messageGroup) {
+        return messageObject;
+    }
+    
+    public static BaseEncryptor getEncryptorBy(long dialogId) {
+        return new BaseEncryptor();
+    }
+    
+    public static int getEncryptorTypeFor(long dialogId) {
+        return 0;
+    }
+    
+    public static void setEncryptorTypeFor(long dialogId, int type) {
+    }
+    
+    public static String encryptMessage(String message, long dialogId, int encryptorType) {
+        return message;
+    }
+    
+    public static void encryptMessage(String message, long dialogId, int encryptorType, Callback callback) {
+        if (callback != null) {
+            callback.onResult(message);
+        }
+    }
+    
+    public interface Callback {
+        void onResult(String result);
+    }
+}
---
+++ TMessagesProj/src/main/java/com/exteragram/messenger/boost/filter/ZalgoFilter.java
@@ -0,0 +1,7 @@
+package com.exteragram.messenger.boost.filter;
+
+public class ZalgoFilter {
+    public static Object filterMessage(String message) {
+        return message;
+    }
+}
